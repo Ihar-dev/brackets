ddc2a6deedbf7a83e701823d1ff219e761140098
Ihar Kozik <ihar.kozik@gmail.com>
2021-08-23 16:34:30 +0300

    feat: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let b = bracketsConfig;
+  arr = str.split("");
+  let l2 = bracketsConfig.length;
+  loop1: for (let i = 0; i < l2; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (b[i][0] == arr[j] && b[i][1] == arr[j + 1]) {
+        arr.splice(j + 1, 1);
+        arr.splice(j, 1);
+        i = -1;
+        continue loop1;
+      }
+    }
+  }
+  return (arr.length > 0) ? false : true;
 }
